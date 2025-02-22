a580d7efd81cd7f57e434502102bae5e53eb07df
to thong dev <thongtodev@gmail.com>
2025-02-22 11:19:55 +0700

    so_sanh

--- test.js
+++ test.js
@@ -1,16 +1,9 @@
 console.log('Hello world')
-let a = {
-    name : `Ray`,
-    class: `TT1`
-};
-let b = 'name';
-a[b] = 'Thong';
-console.log(`My name is: `,a.name);
-console.log(`My class is:`,a.class);
-let c = ['Ray','TT1','Thong'];
-console.log(`Data type of c:`,typeof c,c);
-let rider = [
-    {name : `Geats`,driver: `Desire`},
-    {name : `Lazer`,driver: `Gamer`},
-]
-console.log(`First rider is:`,rider[0].name,`\n second rider is:`,rider[1].name)
+let x = 5; 
+let y = '5';
+
+console.log(' compare x == y: ', 'x = ', x, ' y = ', x, ' result: ', x == y);
+console.log(' compare x === y: ', 'x = ', x, ' y = ', x, ' result: ', x === y);
+
+let z = null;
+console.log('check value: ', z);
