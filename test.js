cf224003529942691456eb258d2a97e14a867ba8
to thong dev <thongtodev@gmail.com>
2025-02-22 10:59:12 +0700

    Array

--- test.js
+++ test.js
@@ -7,3 +7,10 @@ let b = 'name';
 a[b] = 'Thong';
 console.log(`My name is: `,a.name);
 console.log(`My class is:`,a.class);
+let c = ['Ray','TT1','Thong'];
+console.log(`Data type of c:`,typeof c,c);
+let rider = [
+    {name : `Geats`,driver: `Desire`},
+    {name : `Lazer`,driver: `Gamer`},
+]
+console.log(`First rider is:`,rider[0].name,`\n second rider is:`,rider[1].name)
