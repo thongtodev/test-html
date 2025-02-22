9c0a0a3e72341c65b21191b25d64f629fe34e1ff
to thong dev <thongtodev@gmail.com>
2025-02-22 21:19:11 +0700

    for_loop

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-console.log('Hello world')
-let x = 5; 
-let y = '5';
-
-console.log(' compare x == y: ', 'x = ', x, ' y = ', x, ' result: ', x == y);
-console.log(' compare x === y: ', 'x = ', x, ' y = ', x, ' result: ', x === y);
-
-let z = null;
-console.log('check value: ', z);
+let arr = [9,2,2,6,5,3];
+let count = 0;
+let sum = 0;
+for(let i = 0; i < arr.length; i++)
+{
+    if(arr[i] %2 == 0)
+    {
+        count++;
+        sum += arr[i];
+    }
+}
+let avg = sum / count;
+console.log(`Trung binh cac so chan: ${avg}`);
