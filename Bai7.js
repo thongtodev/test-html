98cb30b5458e22dc0aaabea243464e0ac4d963ae
to thong dev <thongtodev@gmail.com>
2025-02-22 10:07:32 +0700

    Bai7

---
+++ Bai7.js
@@ -0,0 +1,5 @@
+console.log('Hello world')
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + 'and\n not' + (2*(a + b))); //Khong xai cach nay
+console.log(`Fifteen is ${a+b} and not ${2*(a+b)}`);
