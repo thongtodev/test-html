1ab212357b6a8a0319e544a6f718d34e9e11cc21
to thong dev <thongtodev@gmail.com>
2025-02-24 09:57:48 +0700

    arrow_function

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
-let a = 29;
-let b = 92;
-function check(a,b)
-{
-  if(a > b)
-  {
-    console.log(`Gia tri cua a lon hon b ${a > b}`);
-  }
-  else
-  {
-    console.log(`Gia tri cua a nho hon b ${a < b}`);
-  }
+let a = 92;
+let b = 29;
+let check = (a,b) => {
+    if(a > b)
+        {
+          console.log(`Gia tri cua a lon hon b ${a > b}`);
+        }
+        else
+        {
+          console.log(`Gia tri cua a nho hon b ${a < b}`);
+        }
 }
 check(a,b);
