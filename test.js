9f39cd290364800144bd20b81de5c6f823e01d10
to thong dev <thongtodev@gmail.com>
2025-02-25 16:50:16 +0700

    lodash_library

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-var arr =[
-  {name:'Geats',power:10000},
-  {name:'Buffa',power:8000},
-  {name:'Nago',power:6000},
-  {name:'Tycoon',power:1000}
-];
-arr.sort(function(a, b) {
-  return b.power - a.power;
-})
 
-console.log(`check power after sort:`,arr);
+var arr =[
+  {subjectId: 1,register: 30},
+  {subjectId: 1,register: 40},
+  {subjectId: 2,register: 35},
+  {subjectId: 2,register: 45}
+];
+var run = _(arr).groupBy('subjectId').map((item, index) => ({
+  subjectId : index,
+  register: _.sumBy(item, 'register')
+})).value();
+console.log(run);
+
