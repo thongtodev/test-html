c726b0784db7f95c5a8b9435af6a2916c55e6c78
to thong dev <thongtodev@gmail.com>
2025-02-25 13:37:23 +0700

    array_map

--- test.js
+++ test.js
@@ -4,12 +4,14 @@ let arr = [
   {name: 'Nago', age:18}
 ]
 
-let filter = arr.filter((item, index) => {
-  return item && item.age === 1000;
-});
+let map = arr.map((item, index) => {
+  console.log(`${item.name} age is ${item.age}`);
+})
 
-let find = arr.find((item, index) => {
-  return item && item.age === 24;
-});
-console.log(`found`, find);
-console.log(`found`,filter);
+let arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let mapNum = arrNum.map((item, index) => {
+  item = item * item;
+  return item;
+})
+console.log(`check value arr: ${arrNum}`);
+console.log(`check value mapNum:`, mapNum);
