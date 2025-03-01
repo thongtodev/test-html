4f057f25e99839921d338b27764e060728891a9d
to thong dev <thongtodev@gmail.com>
2025-03-01 08:36:36 +0700

    01_03_2025

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 
-var arr =[
-  {subjectId: 1,register: 30},
-  {subjectId: 1,register: 40},
-  {subjectId: 2,register: 35},
-  {subjectId: 2,register: 45}
+var arr = [
+  { subjectId: 1, register: 30 },
+  { subjectId: 1, register: 40 },
+  { subjectId: 2, register: 35 },
+  { subjectId: 2, register: 45 }
 ];
 var run = _(arr).groupBy('subjectId').map((item, index) => ({
-  subjectId : index,
+  subjectId: index,
   register: _.sumBy(item, 'register')
 })).value();
 console.log(run);
